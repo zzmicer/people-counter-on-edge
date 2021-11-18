c17804ef7e637c7bf48a98b4c99e1c5fd51c55e6
KastusKalinovski <pazyach@gmail.com>
2021-11-18 03:35:07 +0300

    feat: Add inputs preprocessing

---
+++ src/utils.py
@@ -0,0 +1,14 @@
+import cv2
+
+def preprocessing(input_image, height, width):
+    '''
+    Given an input image, height and width:
+    - Resize to height and width
+    - Transpose the final "channel" dimension to be first
+    - Reshape the image to add a "batch" of 1 at the start 
+    '''
+    image = cv2.resize(input_image, (width, height))
+    image = image.transpose((2,0,1))
+    image = image.reshape(1, 3, height, width)
+
+    return image
